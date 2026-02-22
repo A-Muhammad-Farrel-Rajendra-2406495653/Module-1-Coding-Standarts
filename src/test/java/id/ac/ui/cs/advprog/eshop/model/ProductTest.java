dab466f77826384bbc8c65d0903db3866ec290a2
farrelzzz <rajendramuhammadfarrel@gmail.com>
2026-02-22 13:17:06 +0700

    create unit test for Product model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductTest {
+    Product product;
+    @BeforeEach
+    void setUp() {
+        this.product = new Product();
+        this.product.setProductId("abc");
+        this.product.setProductName("produk1");
+        this.product.setProductQuantity(100);
+    }
+
+    @Test
+    void testGetProductId() {
+        assertEquals("abc", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName() {
+        assertEquals("produk1", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100, this.product.getProductQuantity());
+    }
+}
